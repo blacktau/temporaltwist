61016dd4fed232225c6c52f80f77c300bff7b5d4
Sean Garrett <sean@blacktau.com>
2015-09-19 17:52:05 +0100

    missed file from last commit

---
+++ TemporalTwist/Engine/Steps/FfmpegEncodingStep.cs
@@ -0,0 +1,35 @@
+﻿namespace TemporalTwist.Engine.Steps
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+
+    using Model;
+    using Interfaces.Steps;
+    using Interfaces;
+
+    public class FfmpegEncodingStep : ExternalExecutionStep, IFfmpegEncodingStep
+    {
+        public FfmpegEncodingStep(IConsoleOutputBus consoleOutputHandler)
+            : base("ffmpeg.exe", consoleOutputHandler)
+        {
+        }
+
+        public override void ProcessItem(IJob job, IJobItem item)
+        {
+            var bitRate = job.Format.BitRate;
+            var sampleRate = job.Format.SampleRate;
+            var tempdir = Path.GetTempPath();
+            var tempfile = Path.Combine(tempdir, DateTime.Now.Ticks + "." + job.Format.Extension);
+            var exec = string.Format(
+                CultureInfo.InvariantCulture, 
+                "-i \"{0}\" -ab {1} -ar {2} -ac 2 \"{3}\"", 
+                item.LastFile, 
+                bitRate, 
+                sampleRate, 
+                tempfile);
+            this.Execute(exec);
+            item.TemporaryFiles.Add(tempfile);
+        }
+    }
+}
